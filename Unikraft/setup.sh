5b35235b1cdc89bf25878918422f334193c5b712
m <ge78pej@mytum.de>
2025-08-22 14:01:08 +0200

    Check for kvm support before running qemu

--- Unikraft/setup.sh
+++ Unikraft/setup.sh
@@ -32,20 +32,21 @@ cat << EOF > $SCRIPT_DIR/openssl
 #!/usr/bin/env bash
 SCRIPT_PATH=\$(realpath "\$0")
 SCRIPT_DIR=\$(dirname "\$SCRIPT_PATH")
-
+if [ -e "/dev/kvm" ]; then
+    KVM_SUPPORT="-enable-kvm"
+fi
+SEED=\$(hexdump -vn32 -e'8/4 "0x%08X "' /dev/urandom)
 qemu-system-aarch64 \\
-    -enable-kvm \\
 	-kernel \$SCRIPT_DIR/ssl_uk_bin/build/ssl_uk_qemu-arm64 \\
 	-machine virt -cpu max -m 128M \\
 	-nographic \\
 	-netdev bridge,id=en0,br=virbr0 \\
     -device virtio-net-pci,netdev=en0 \\
-	-append "ssl_uk netdev.ip=172.44.0.2/24:172.44.0.1::: vfs.fstab=[ \"fs0:/:9pfs:::\" ] -- \$*" \\
+	-append "ssl_uk random.seed=[\${SEED}] netdev.ip=172.44.0.2/24:172.44.0.1::: vfs.fstab=[ \"fs0:/:9pfs:::\" ] -- \$*" \\
     -fsdev local,id=myid,path=\$(pwd),security_model=none \\
-    -device virtio-9p-pci,fsdev=myid,mount_tag=fs0
+    -device virtio-9p-pci,fsdev=myid,mount_tag=fs0 \\
+    \$KVM_SUPPORT
 EOF
-
-chmod a+x $SCRIPT_DIR/openssl
 
 cat << EOF > $SCRIPT_DIR/test
 #!/usr/bin/env bash
@@ -58,23 +59,26 @@ qemu-system-aarch64 \\
     -nographic
 EOF
 
-chmod a+x $SCRIPT_DIR/test
-
-
 cat << EOF > $SCRIPT_DIR/benchmark
 #!/usr/bin/env bash
 SCRIPT_PATH=\$(realpath "\$0")
 SCRIPT_DIR=\$(dirname "\$SCRIPT_PATH")
+if [ -e "/dev/kvm" ]; then
+    KVM_SUPPORT="-enable-kvm"
+fi
 SEED=\$(hexdump -vn32 -e'8/4 "0x%08X "' /dev/urandom)
 qemu-system-aarch64 \\
-    -enable-kvm \\
     -append "ssl_uk random.seed=[\${SEED}] -- \$*" \\
     -kernel \$SCRIPT_DIR/oqs_uk_speed/build/ssl_uk_qemu-arm64 \\
     -append " -- \$*" \\
     -machine virt -cpu max -m 1G \\
-    -nographic
+    -nographic \\
+    \$KVM_SUPPORT
 EOF
 
+
+chmod a+x $SCRIPT_DIR/test
+chmod a+x $SCRIPT_DIR/openssl
 chmod a+x $SCRIPT_DIR/benchmark
 
 
